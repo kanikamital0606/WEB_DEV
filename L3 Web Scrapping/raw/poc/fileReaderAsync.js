5d8731b05f4526232cef66336a07d7a201b2f1b7
Kanika Mittal <kanikamittal0661996@gmail.com>
2020-05-30 22:42:01 +0530

    update

---
+++ L3 Web Scrapping/raw/poc/fileReaderAsync.js
@@ -0,0 +1,10 @@
+//Nodejs => default async
+
+let fs = require("fs");
+console.log("started reading file");
+fs.readFile("index.txt", function(err,content){
+    console.log(content+ " ");
+    console.log("all content read");
+})
+
+console.log("I can do other things");
